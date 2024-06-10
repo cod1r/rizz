30f58850d396619919cce326ede2cde08387a488
cod1r <jasonho353@gmail.com>
2024-06-10 10:21:35 -0500

    audio basics and tailwind and basic code

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Fourier Transform shenanigans
 
 - Just me messing around with fourier transforms.
+
+
+[Mathologer](https://www.youtube.com/watch?v=-dhHrg-KbJ0)
+
+ - According to this video, you can refactor e^(pi * i) into (1 + pi/(n * pi)) ^ (n * pi) as n goes to infinity.
+ 1. The (pi / (n * pi)) is basically 1 / n.
+ 2. n * pi can be some number m.
+ 3. so we have (1 + pi / m) ^ m as m goes to infinity; which is something similar to what we have above.
+ 4. pi is 3.14... but can represent any number x.
+ 5. so now we have (1 + x / m) ^ m. again something similar to the equations above
+ 6. plug in pi * i as x as m goes to infinity and the final answer should come really close or approach -1
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+function useAudio(performFourier: boolean) {
+  const audioContext = useRef<AudioContext | null>(null);
+  const audio = useRef<HTMLAudioElement | null>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    audioContext.current = new AudioContext();
+    audio.current = new Audio();
+    audioContext.current.createMediaElementSource(audio.current);
+  }, []);
+
+  useEffect(() => {
+    if (performFourier) {
+    }
+  }, [performFourier]);
+
+  return audio.current;
 }
 
-export default App
+function App() {
+  const inputFile = useRef<HTMLInputElement>(null);
+  const [submitted, setSubmitted] = useState(false);
+  const [performFourier, setPerformFourier] = useState(false);
+
+  const audio = useAudio(performFourier);
+  return (
+    <div className="flex items-center justify-center w-full h-full">
+      <div>
+        <button
+          style={{
+            textAlign: "center",
+          }}
+          onClick={() => {
+            if (submitted) {
+              setPerformFourier(true)
+              return;
+            }
+            if (inputFile.current === null) return;
+            inputFile.current.click();
+          }}
+        >
+          {submitted ? "Perform FT" : "Choose audio file"}
+        </button>
+        <input
+          onInput={(e) => {
+            setSubmitted(true);
+            if (audio !== null) {
+              audio.src = URL.createObjectURL(e.currentTarget.files![0]);
+            }
+          }}
+          ref={inputFile}
+          style={{ display: "none" }}
+          type="file"
+          id="fileInput"
+          name="fileInput"
+          accept=".mp3,.flac,.wav"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
